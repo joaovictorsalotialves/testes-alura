f9fd15959d73034c094039d23b35e3551cc88c1d
João Victor <joaovictorsalotialves12@gmail.com>
2026-01-12 20:07:52 -0300

    Asserções

--- src/index.js
+++ src/index.js
@@ -2,14 +2,36 @@ const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtra
 
 const calculaDescontos = (salario, descontos) => salario - descontos
 
-const teste = (titulo, esperado, retornado) => {
-  if (esperado === retornado) {
-    console.log(`${titulo} passou`)
-  } else {
-    console.error(`${titulo} não passou`)
+const verifiqueSe = valor => {
+  const assercoes = {
+    eExatamenteIgualA(esperado) {
+      if (valor !== esperado) {
+        throw {}
+      }
+    },
+  }
+  return assercoes
+}
+
+const teste = (titulo, funcaoDeTeste) => {
+  try {
+    funcaoDeTeste()
+    console.log(`✅ ${titulo}`)
+  } catch {
+    console.error(`❌ ${titulo}`)
   }
 }
 
-teste('somaHorasExtras', 2500, somaHorasExtras(2000, 500))
+teste('somaHorasExtras', () => {
+  const esperado = 2500
+  const retornado = somaHorasExtras(2000, 500)
 
-teste('calculaDesconto', 2200, calculaDescontos(2500, 300))
+  verifiqueSe(retornado).eExatamenteIgualA(esperado)
+})
+
+teste('calculaDesconto', () => {
+  const esperado = 2300
+  const retornado = calculaDescontos(2500, 200)
+
+  verifiqueSe(retornado).eExatamenteIgualA(esperado)
+})

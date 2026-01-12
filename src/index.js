07c62804d479fadfb62b99b0428e3902a6a83933
João Victor <joaovictorsalotialves12@gmail.com>
2026-01-12 20:19:56 -0300

    Apresentando o Jest

--- src/index.js
+++ src/index.js
@@ -2,36 +2,4 @@ const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtra
 
 const calculaDescontos = (salario, descontos) => salario - descontos
 
-const verifiqueSe = valor => {
-  const assercoes = {
-    eExatamenteIgualA(esperado) {
-      if (valor !== esperado) {
-        throw {}
-      }
-    },
-  }
-  return assercoes
-}
-
-const teste = (titulo, funcaoDeTeste) => {
-  try {
-    funcaoDeTeste()
-    console.log(`✅ ${titulo}`)
-  } catch {
-    console.error(`❌ ${titulo}`)
-  }
-}
-
-teste('somaHorasExtras', () => {
-  const esperado = 2500
-  const retornado = somaHorasExtras(2000, 500)
-
-  verifiqueSe(retornado).eExatamenteIgualA(esperado)
-})
-
-teste('calculaDesconto', () => {
-  const esperado = 2300
-  const retornado = calculaDescontos(2500, 200)
-
-  verifiqueSe(retornado).eExatamenteIgualA(esperado)
-})
+export { somaHorasExtras, calculaDescontos }

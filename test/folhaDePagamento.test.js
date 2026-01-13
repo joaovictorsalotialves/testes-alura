9263daf99a0b74ec4ce649357371ea879efca12a
João Victor <joaovictorsalotialves12@gmail.com>
2026-01-13 00:27:09 -0300

    Relatórios

--- test/folhaDePagamento.test.js
+++ test/folhaDePagamento.test.js
@@ -1,15 +1,17 @@
 import { calculaDescontos, somaHorasExtras } from "../src/index"
 
-test('Deve retornar a soma das horas extras', () => {
-  const esperado = 2500
-  const retornado = somaHorasExtras(2000, 500)
+describe('Testes dos calculos de folha de pagamento', () => {
+  it('Deve retornar a soma das horas extras', () => {
+    const esperado = 2500
+    const retornado = somaHorasExtras(2000, 500)
 
-  expect(retornado).toBe(esperado)
-})
+    expect(retornado).toBe(esperado)
+  })
 
-test('Deve descontar o valor das horas extras', () => {
-  const esperado = 2300
-  const retornado = calculaDescontos(2500, 200)
+  it('Deve descontar o valor das horas extras', () => {
+    const esperado = 2300
+    const retornado = calculaDescontos(2500, 200)
 
-  expect(retornado).toBe(esperado)
+    expect(retornado).toBe(esperado)
+  })
 })
